977244b7d5fc1647f008cd667905e85cef00c828
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-14 14:57:31 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querySelector('form');
+  submit.addEventListener("submit", (e) => {
+    e.preventDefault()
+    createToDo()
+    submit.reset()
+  
+});
+
+function createToDo(){
+  const input = document.getElementById("new-task-description");
+  const newTask = document.getElement('li');
+  const deleteBtn = document.createElement('button');
+  newTask.textContent = `${input,value}`;
+  document.getElementById('task').appendChild(newTask);
+
+
+  deleteBtn.textContent= "DONE";
+  newTask.appendChild(deleteBtn);
+  deleteBtn.addEventListener('click', () => newTask.remove())
+}
+
+
 });
